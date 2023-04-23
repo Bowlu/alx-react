850ff0642c4c20fb1092ebb0760e5799b759618f
Bowlu <awoyemiboluwatife@gmail.com>
2023-04-23 17:47:57 +0100

    task 1

---
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,18 @@
+import $ from 'jquery';
+import _ from 'lodash';
+
+$(document).ready(function() {
+  $('body').append('<p>Holberton Dashboard</p>');
+  $('body').append('<p>Dashboard data for the students</p>');
+  $('body').append('<button>Click here to get started</button>');
+  $('body').append('<p id="count"></p>');
+  $('body').append('<p>Copyright - Holberton School</p>');
+});
+
+let numberCount = 0;
+function updateCounter() {
+  $('#count').html(`${numberCount} clicks on the button`);
+  numberCount++;
+}
+
+$('button').on('click', _debounce(updateCounter, 100));
