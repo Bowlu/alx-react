1ef386536225c8bdc60ff6302e97fb408eca22f5
Bowlu <awoyemiboluwatife@gmail.com>
2023-04-24 11:52:55 +0100

    installing enzyme

--- 0x01-react_intro/task_3/dashboard/src/utils.js
+++ 0x01-react_intro/task_3/dashboard/src/utils.js
@@ -1,5 +1,5 @@
-export const getFullYear = () => {new date().getFullYear();}
+export const getFullYear = () => {new Date().getFullYear();}
 
-export const getFootercopy = (isIndex) => {isIndex ? "Holberton School" : "Holberton School main dashboard"}
+export const getFooterCopy = (isIndex) => isIndex ? "Holberton School" : "Holberton School main dashboard";
 
 export const getLatestNotification = () => "<strong>Urgent requirement</strong> - complete by EOD";
