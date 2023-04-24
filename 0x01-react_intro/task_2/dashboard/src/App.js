322b74fde242364d0f68da0bdcd3292097a675c9
Bowlu <awoyemiboluwatife@gmail.com>
2023-04-24 10:36:09 +0100

    added getlatest notification function

--- 0x01-react_intro/task_2/dashboard/src/App.js
+++ 0x01-react_intro/task_2/dashboard/src/App.js
@@ -12,6 +12,11 @@ function App() {
 
       <body className="App-body">
         <p>Login to access the full dashboard</p>
+        <label htmlFor="email">Email:</label>
+        <input type="email" name="email" id="email"></input>
+        <label htmlFor="password">Password:</label>
+        <input type="password" name="password" id="password"></input>
+        <button>OK</button> 
       </body>
       <hr className="line"></hr>
 
